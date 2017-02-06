9532828ec050353d7eca3e4be34c71ee619dc124
Soar Qin <soarchin@gmail.com>
2017-02-07 02:16:03 +0800

    added string searcher

---
+++ nis_txt/search.cc
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <map>
+#include <set>
+
+#include "utf.hh"
+
+static std::map<uint32_t, uint16_t> cp;
+static std::set<uint32_t> uset;
+
+inline void load_codepage(const char* fn) {
+    FILE* f = fopen(fn, "rt");
+    char line[256];
+    while(fgets(line, 256, f) != nullptr) {
+        char* t = strtok(line, "=");
+        if (t == nullptr) continue;
+        uint32_t key = (uint32_t)strtoul(t, nullptr, 16);
+        t = strtok(nullptr, "=");
+        if (t == nullptr) continue;
+        uint8_t s[7];
+        int i = 0;
+        while(i < 6 && (uint8_t)*t >= 0x20) {
+            s[i] = (uint8_t)*t;
+            ++t;
+            ++i;
+        }
+        uint16_t o[7];
+        int outlen = 6;
+        int inlen = i;
+        if(utf8_to_utf16le(o, &outlen, s, &inlen) == 0) {
+            cp[key] = o[0];
+            uset.insert(o[0]);
+        }
+    }
+    fclose(f);
+}
+
+inline static uint8_t* read_file(const char* fn, size_t& sz) {
+    FILE* f = fopen(fn, "rb");
+    if (f == nullptr) {
+        return nullptr;
+        sz = 0;
+    }
+    fseek(f, 0, SEEK_END);
+    sz = ftell(f);
+    if (sz == 0) {
+        fclose(f);
+        return nullptr;
+    }
+    fseek(f, 0, SEEK_SET);
+    uint8_t* data = new uint8_t[sz];
+    fread(data, 1, sz, f);
+    fclose(f);
+    return data;
+}
+
+inline bool valid_str(const char* string, uint16_t* output) {
+    const uint8_t* ptr = (const uint8_t*)string;
+    uint16_t* optr = output;
+    while(*ptr != 0) {
+        uint8_t c = *ptr++;
+        if(c < 0x80) {
+            if(optr) *optr++ = c;
+        } else {
+            uint32_t r = ((uint32_t)c << 8) | (*ptr++);
+            auto ite = cp.find(r);
+            if(ite == cp.end()) return false;
+            if(optr) *optr++ = ite->second;
+        }
+    }
+    *optr = 0;
+    return true;
+}
+
+
+int main(int argc, char* argv[]) {
+    bool u8 = false;
+    if (argc > 2) {
+        load_codepage(argv[2]);
+    } else u8 = true;
+    size_t sz;
+    uint8_t* data = read_file(argv[1], sz);
+    if (data == nullptr) return -1;
+    uint8_t* rdata = new uint8_t[sz];
+    for (size_t i = 0; i < sz; ++i)
+        rdata[i] = data[i] < 0x20 ? 0 : data[i];
+    if (u8) {
+        for (size_t i = 0; i < sz; ++i) {
+            const char* s = (const char*)rdata + i;
+            uint32_t cnt[4] = {0, 0, 0, 0};
+            if(is_utf8(s, cnt)) {
+                uint32_t l = (uint32_t)strlen(s);
+                if (l < 3 || cnt[2] + cnt[3] + cnt[4] == 0 || data[i + l] != 0) {
+                    continue;
+                }
+                size_t j = i;
+                uint32_t ll = l;
+                while (ll >= 3 && *(rdata + j) == 0xE3 && *(rdata + j + 1) == 0x80 && *(rdata + j + 2) == 0x80) {
+                    ll -= 3; j += 3;
+                }
+                if (ll < 3) continue;
+                fprintf(stdout, "0x%08X,%u,%s\n", i, l, s);
+                i += l;
+            }
+        }
+    } else {
+        for (size_t i = 0; i < sz; ++i) {
+            const char* s = (const char*)rdata + i;
+            uint16_t out[32768];
+            if(valid_str(s, out)) {
+                uint32_t l = (uint32_t)strlen(s);
+                if (l < 2 || data[i + l] != 0) {
+                    continue;
+                }
+                int inlen = wcslen((wchar_t*)out);
+                uint32_t cnt = 0;
+                for(int j = 0; j < inlen; ++j) {
+                    if(out[j] > 0x80) ++cnt;
+                }
+                if (cnt == 0) continue;
+                uint8_t* uout = new uint8_t[inlen * 4 + 1];
+                int outlen = inlen * 4 + 1;
+                if (utf16le_to_utf8(uout, &outlen, out, &inlen) != 0) continue;
+                if (outlen == 0) continue;
+                uout[outlen] = 0;
+                fprintf(stdout, "0x%08X,%u,%s\n", i, l, uout);
+                i += l;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ nis_txt/utf.hh
@@ -0,0 +1,234 @@
+#pragma once
+
+#include <string.h>
+
+inline bool is_utf8(const char* string, uint32_t* count) {
+    if(!string)
+        return 0;
+
+    const unsigned char* bytes = (const unsigned char*)string;
+    while(*bytes) {
+        if( (// ASCII
+             // use bytes[0] <= 0x7F to allow ASCII control characters
+                bytes[0] == 0x09 ||
+                bytes[0] == 0x0A ||
+                bytes[0] == 0x0D ||
+                (0x20 <= bytes[0] && bytes[0] <= 0x7E)
+            )
+        ) {
+            bytes += 1;
+            if(count) ++count[0];
+            continue;
+        }
+
+        if( (// non-overlong 2-byte
+                (0xC2 <= bytes[0] && bytes[0] <= 0xDF) &&
+                (0x80 <= bytes[1] && bytes[1] <= 0xBF)
+            )
+        ) {
+            bytes += 2;
+            if(count) ++count[1];
+            continue;
+        }
+
+        if( (// excluding overlongs
+                bytes[0] == 0xE0 &&
+                (0xA0 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
+            ) ||
+            (// straight 3-byte
+                ((0xE1 <= bytes[0] && bytes[0] <= 0xEC) ||
+                    bytes[0] == 0xEE ||
+                    bytes[0] == 0xEF) &&
+                (0x80 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
+            ) ||
+            (// excluding surrogates
+                bytes[0] == 0xED &&
+                (0x80 <= bytes[1] && bytes[1] <= 0x9F) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
+            )
+        ) {
+            bytes += 3;
+            if(count) ++count[2];
+            continue;
+        }
+
+        if( (// planes 1-3
+                bytes[0] == 0xF0 &&
+                (0x90 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
+                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
+            ) ||
+            (// planes 4-15
+                (0xF1 <= bytes[0] && bytes[0] <= 0xF3) &&
+                (0x80 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
+                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
+            ) ||
+            (// plane 16
+                bytes[0] == 0xF4 &&
+                (0x80 <= bytes[1] && bytes[1] <= 0x8F) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
+                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
+            )
+        ) {
+            bytes += 4;
+            if(count) ++count[3];
+            continue;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+inline static int utf8_to_utf16le(uint16_t* out, int *outlen,
+            const uint8_t* in, int *inlen)
+{
+    const uint8_t* processed = in;
+    const uint8_t *const instart = in;
+    uint16_t* outstart = out;
+    uint16_t* outend;
+    const uint8_t* inend= in+*inlen;
+    uint32_t c, d;
+    int trailing;
+    uint8_t *tmp;
+    uint16_t tmp1, tmp2;
+
+    /* UTF16LE encoding has no BOM */
+    if (in == NULL) {
+        *outlen = 0;
+        *inlen = 0;
+        return(0);
+    }
+    outend = out + *outlen;
+    while (in < inend) {
+        d= *in++;
+        if      (d < 0x80)  { c= d; trailing= 0; }
+        else if (d < 0xC0) {
+            /* trailing byte in leading position */
+            *outlen = out - outstart;
+            *inlen = processed - instart;
+            return(-2);
+        } else if (d < 0xE0)  { c= d & 0x1F; trailing= 1; }
+        else if (d < 0xF0)  { c= d & 0x0F; trailing= 2; }
+        else if (d < 0xF8)  { c= d & 0x07; trailing= 3; }
+        else {
+            /* no chance for this in UTF-16 */
+            *outlen = out - outstart;
+            *inlen = processed - instart;
+            return(-2);
+        }
+
+        if (inend - in < trailing) {
+            break;
+        } 
+
+        for ( ; trailing; trailing--) {
+            if ((in >= inend) || (((d= *in++) & 0xC0) != 0x80))
+                break;
+            c <<= 6;
+            c |= d & 0x3F;
+        }
+
+        /* assertion: c is a single UTF-4 value */
+        if (c < 0x10000) {
+            if (out >= outend)
+                break;
+            *out++ = c;
+        } else if (c < 0x110000) {
+            if (out+1 >= outend)
+            break;
+            c -= 0x10000;
+            *out++ = 0xD800 | (c >> 10);
+            *out++ = 0xDC00 | (c & 0x03FF);
+        }
+        else
+            break;
+        processed = in;
+    }
+    *outlen = out - outstart;
+    *inlen = processed - instart;
+    return(0);
+}
+
+inline static int utf16le_to_utf8(uint8_t* out, int *outlen,
+            const uint16_t* in, int *inlen)
+{
+    uint8_t* outstart = out;
+    const uint16_t* processed = in;
+    uint8_t* outend = out + *outlen;
+    const uint16_t* inend;
+    uint32_t c, d;
+    uint8_t *tmp;
+    int bits;
+
+    inend = in + *inlen;
+    while ((in < inend) && (out - outstart + 5 < *outlen)) {
+        c= *in++;
+        if ((c & 0xFC00) == 0xD800) {    /* surrogates */
+            if (in >= inend) {           /* (in > inend) shouldn't happens */
+                break;
+            }
+            d = *in++;
+            if ((d & 0xFC00) == 0xDC00) {
+                c &= 0x03FF;
+                c <<= 10;
+                c |= d & 0x03FF;
+                c += 0x10000;
+            } else {
+                *outlen = out - outstart;
+                *inlen = processed - in;
+                return(-2);
+            }
+        }
+
+        /* assertion: c is a single UTF-4 value */
+        if (out >= outend)
+            break;
+        if      (c <    0x80) {  *out++=  c;                bits= -6; }
+        else if (c <   0x800) {  *out++= ((c >>  6) & 0x1F) | 0xC0;  bits=  0; }
+        else if (c < 0x10000) {  *out++= ((c >> 12) & 0x0F) | 0xE0;  bits=  6; }
+        else                  {  *out++= ((c >> 18) & 0x07) | 0xF0;  bits= 12; }
+ 
+        for ( ; bits >= 0; bits-= 6) {
+            if (out >= outend)
+                break;
+            *out++= ((c >> bits) & 0x3F) | 0x80;
+        }
+        processed = in;
+    }
+    *outlen = out - outstart;
+    *inlen = processed - in;
+    return(0);
+}
+
+inline static uint32_t utf8_to_utf16le(uint32_t c) {
+    uint8_t chars[5] = {(uint8_t)c, (uint8_t)(c >> 8), (uint8_t)(c >> 16), (uint8_t)(c >> 24), 0};
+    int inlen = strlen((const char*)chars);
+    uint16_t out[5];
+    int outlen = 5;
+    if(utf8_to_utf16le(out, &outlen, chars, &inlen) == 0) {
+        // printf("%X %X\n", c, out[0]);
+        return out[0];
+    }
+    return 0;
+}
+
+inline static uint32_t utf16le_to_utf8(uint32_t c) {
+    uint16_t chars[2] = {(uint16_t)c, 0};
+    int inlen = 1;
+    uint8_t out[8] = {};
+    int outlen = 8;
+    if(utf16le_to_utf8(out, &outlen, chars, &inlen) == 0) {
+        uint32_t r = 0;
+        for(uint8_t i = 0; i < 4; ++i) {
+            if(out[i] == 0) break;
+            r |= ((uint32_t)out[i]) << (8 * i);
+        }
+        return r;
+    }
+    return 0;
+}
